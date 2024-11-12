d87aa37157ee83a9951cf3b22e70e69b9bdf6b46
Andrew Levin <andrewlevingit@hotmail.com>
2024-11-12 03:07:59 +0300

    2024-11-12 03:07:59 N

--- docker-runner-bot.sh
+++ docker-runner-bot.sh
@@ -11,24 +11,17 @@ SILENT_MODE="${SILENT_MODE:-false}"
 
 LOG_TO_FILE="${LOG_TO_FILE:-false}"
 
-# Function to log messages based on silent mode
-log_message() {
-    if [ "$SILENT_MODE" != true ]; then
-        echo "[docker-runner-bot.sh] $1"
-    fi
-}
-
-log_message "SILENT_MODE is set to $SILENT_MODE."
-log_message "To change the silent mode setting, set the environment variable SILENT_MODE to true or false."
+echo "[docker-runner-bot.sh] SILENT_MODE is set to $SILENT_MODE."
+echo "[docker-runner-bot.sh] ❗️ To change the silent mode setting, set the environment variable SILENT_MODE to true or false."
 
 # Execute the ytb2audiobot command with or without output suppression
 if [ "$SILENT_MODE" = true ]; then
-    log_message "Silent mode is ON. The command will run without log output."
+    echo "[docker-runner-bot.sh] 🔐 Silent mode is ON. The command will run without log output."
     ytb2audiobot > /dev/null 2>&1
 elif [ "$LOG_TO_FILE" = true ]; then
-    log_message "Logging mode is ON. The command output will be saved to output.log."
+    echo "[docker-runner-bot.sh] 💾 Logging mode is ON. The command output will be saved to output.log."
     ytb2audiobot | tee -a output.log
 else
-    log_message "Silent mode is OFF. Running command with log output."
+    echo "[docker-runner-bot.sh] 📟 Silent mode is OFF. Running command with standart log output."
     ytb2audiobot
 fi
