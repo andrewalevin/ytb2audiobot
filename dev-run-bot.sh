659905b3aa21a1e875040444477203f968f4fb12
Andrew Levin <andrewlevingit@hotmail.com>
2025-01-15 05:57:39 +0300

    2025-01-15 05:57:39 N

--- dev-run-bot.sh
+++ dev-run-bot.sh
@@ -4,6 +4,13 @@
 pip install -e . --no-deps
 
 # shellcheck disable=SC2046
-export $(grep -v '^#' .env | xargs)
+# export $(grep -v '^#' .env | xargs)
+
+
+# Load the environment variables from the .env file, ignoring comments and handling special characters properly
+set -a  # Automatically export all variables defined
+source .env
+set +a  # Disable automatic export
+
 
 ytb2audiobot 2>&1 | tee -a output.log
